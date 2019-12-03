4c1496cb28090d6b7e347be18ac2ed6542830ad7
wzrd13 <42070629+wzrd13@users.noreply.github.com>
2019-12-03 21:31:35 +0200

    did some changes

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dot_list=(".bashrc" ".zshrc" ".bash_profile" ".tmux.conf" ".vimrc")
+dot_list=(".bashrc" ".zshrc" ".bash_profile" ".tmux.conf" ".vimrc" ".gitconfig")
 
 for file in $dot_list; do
 	ln -sv "$PWD/$file" "$HOME"
